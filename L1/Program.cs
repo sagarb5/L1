e5b62082708195a3d3bbf78e35cfb3bfbe4e95df
sagar26197@gmail.com <sagar26197@gmail.com>
2019-01-22 18:21:54 -0500

    V2

--- L1/Program.cs
+++ L1/Program.cs
@@ -6,7 +6,24 @@ namespace L1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type in the values");
+            Console.WriteLine("Side 1");
+            string input = Console.ReadLine();
+            double a = Double.Parse(input);
+
+            Console.WriteLine("Side 2");
+            string input1 = Console.ReadLine();
+            double b = Double.Parse(input1);
+
+            Console.WriteLine("Side 3");
+            string input2 = Console.ReadLine();
+            double c = Double.Parse(input2);
+
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            Console.WriteLine("Area of triangle is = {0}", +area);
+            Console.ReadLine();
+            
         }
     }
 }
